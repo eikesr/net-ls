fcc246a447e337f3b88695143db76f4d2dfdfb23
eikesr <eike.roehrs@googlemail.com>
2018-06-02 15:04:17 +0200

    net-ls.py now takes arguments
    The example application net-ls.py now requires an IP address as input.
    Additionally the number of bits in the netmask of the network can
    optionally be specified.

--- net-ls.py
+++ net-ls.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 import list_network_devices as lsn
+import argparse
 
-def main():
-    network_devices = lsn.list_network_devices('192.168.1.0', 24)
+def main(ip, netmask_bits):
+    network_devices = lsn.list_network_devices(ip, netmask_bits)
     print(network_devices)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser('List IP and hostname of devices in the network')
+    parser.add_argument('ip', type=str, help='IPv4 base address of the network (e.g., 192.168.0.0)')
+    parser.add_argument('--netmask_bits', default=24, type=int, required=False, help='Number of bits of the netmask. For local networks typically 24.')
+
+    args = parser.parse_args()
+    main(args.ip, args.netmask_bits)
 
